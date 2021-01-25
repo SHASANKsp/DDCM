7690004d3f0ecc09cdfe87e1e93bd99b53bb0218
SHASANKsp <58630523+SHASANKsp@users.noreply.github.com>
2021-01-25 19:29:46 +0530

    Update SUBMIT-data.php

--- SUBMIT-data.php
+++ SUBMIT-data.php
@@ -5,16 +5,18 @@ $con = mysqli_connect("localhost","root","","DDCM") or die("Failed to connect to
 $name = $_POST['Name'];
 $email = $_POST['Email'];
 $Comments = $_POST['Comments'];
+$filename = $_FILES['Data']['name'];
+$tmppath = $_FILES['Data']['tmp_name'];
+$targetpath = "F:/Software bioinfo/GIT dir/DDCM/All_data/".$filename; 
 
-
-
-$qu = "INSERT INTO `km-data`
-  (`NAME`, `EMAIL`,`COMMENTS`)
+$qu = "INSERT INTO `submit-data`
+  (`NAME`, `EMAIL`,`COMMENTS`,`FILE`)
   VALUES
-  ('$name', '$email','$Comments');";
+  ('$name', '$email','$Comments','$filename');";
 
 
 if($con->multi_query($qu) == TRUE){
+  move_uploaded_file($tmppath,$targetpath);
   echo "Thank you for submiting your data, our database will keep expanding, all because of contributers like you. <br><br><br>";
   echo "We will check all the aspects and will get back to you.<br>";
 }else{
