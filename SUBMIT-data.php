612bb278b645c314a7713d4df3bfa6b91066381f
SHASANKsp <58630523+SHASANKsp@users.noreply.github.com>
2021-02-14 19:00:23 +0530

    Update SUBMIT-data.php

--- SUBMIT-data.php
+++ SUBMIT-data.php
@@ -1,3 +1,31 @@
+<html>
+
+<head>
+    
+    <title>
+        SEARCH RESULT - DDCM
+    </title>
+    <link href="Page design.css" rel="stylesheet">    
+</head>
+
+<body class="body">
+    <h1 style="background-color: brown;">
+    <table style="color: white;">
+    <tr>
+        <th style="font-size: xx-large; ">DDCM</th>
+        <th style="text-align: left;">| Details on<br>| Disease<br>| Causing<br>| Mutations</th>
+        <th style="color: brown;">----------</th>
+        <th><a  style="color: white;" href="HOME.html"> HOME </a></th>
+        <th style="color: brown;">----------</th>
+        <th><a  style="color: white;" href="GENETIC_DISEASE.html"> GENETIC DISEASE  </a></th>
+        <th style="color: brown;">----------</th>
+        <th><a  style="color: white;" href="SUBMIT.html"> SUBMIT </a></th>
+        <th style="color: brown;">----------</th>
+        <th><a  style="color: white;" href="FEEDBACK.html"> FEEDBACK </a></th>
+    </tr>
+    </table>
+    </h1>
+
 <?php
 
 $con = mysqli_connect("localhost","root","","DDCM") or die("Failed to connect to MySQL:".mysqli_error());
@@ -25,3 +53,8 @@ if($con->multi_query($qu) == TRUE){
 
 
 ?>
+
+
+  <h1 style="color: brown; background-color: brown;">END</h1>
+</body>
+</html>
